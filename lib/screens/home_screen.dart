e978744a7ee7eee214e461b49ed462817178cd79
dtlucho <luciano.ditomaso@gmail.com>
2021-12-12 16:50:56 -0300

    Home screen basic layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movies/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movies in theatres"),
+        elevation: 0.0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          CardSwiper(),
+          // horizontal movie list
+        ],
       ),
     );
   }
