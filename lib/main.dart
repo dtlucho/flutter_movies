2bce699b8fcc1fd28c7154aeb1411c954a82042a
dtlucho <luciano.ditomaso@gmail.com>
2021-12-20 14:57:48 -0300

    Initializing provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:flutter_movies/providers/movies_provider.dart';
 import 'package:flutter_movies/screens/screens.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MoviesProvider(),
+          lazy: false,
+        ),
+      ],
+      child: const MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -19,7 +37,7 @@ class MyApp extends StatelessWidget {
         'details': (_) => const DetailsScreen(),
       },
       theme: ThemeData.light().copyWith(
-          appBarTheme: AppBarTheme(
+          appBarTheme: const AppBarTheme(
         color: Colors.indigo,
       )),
     );
