b66b160c44e6ed688f9508111daaa376751b017d
dtlucho <luciano.ditomaso@gmail.com>
2021-12-26 19:13:54 -0300

    using movie list to render card swiper

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
+import 'package:flutter_movies/models/models.dart';
 
 class CardSwiper extends StatelessWidget {
-  const CardSwiper({Key? key}) : super(key: key);
+  const CardSwiper({
+    Key? key,
+    required this.movies,
+  }) : super(key: key);
+
+  final List<Movie> movies;
 
   @override
   Widget build(BuildContext context) {
@@ -11,11 +17,12 @@ class CardSwiper extends StatelessWidget {
       width: double.infinity,
       height: size.height * 0.5,
       child: Swiper(
-        itemCount: 10,
+        itemCount: movies.length,
         layout: SwiperLayout.STACK,
         itemWidth: size.width * 0.6,
         itemHeight: size.height * 0.4,
         itemBuilder: (_, int index) {
+          final movie = movies[index];
           return GestureDetector(
             onTap: () => Navigator.pushNamed(
               context,
@@ -24,9 +31,9 @@ class CardSwiper extends StatelessWidget {
             ),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20.0),
-              child: const FadeInImage(
-                placeholder: AssetImage('assets/no-image.jpg'),
-                image: NetworkImage('https://via.placeholder.com/300x400'),
+              child: FadeInImage(
+                placeholder: const AssetImage('assets/no-image.jpg'),
+                image: NetworkImage(movie.fullPosterImg),
                 fit: BoxFit.cover,
               ),
             ),
