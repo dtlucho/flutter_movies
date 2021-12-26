dd750c3a6c15e1dc2915400b7b2f66b0401ada25
dtlucho <luciano.ditomaso@gmail.com>
2021-12-26 19:13:25 -0300

    using movie provider in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movies/providers/movies_provider.dart';
 import 'package:flutter_movies/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Movies in theatres"),
@@ -19,9 +23,11 @@ class HomeScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: const [
-            CardSwiper(),
-            MovieSlider(),
+          children: [
+            CardSwiper(
+              movies: moviesProvider.nowPlayingMovies,
+            ),
+            const MovieSlider(),
           ],
         ),
       ),
