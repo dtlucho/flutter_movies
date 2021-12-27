f530afce16de8d09b9699bdcc91dd3851c861fb9
dtlucho <luciano.ditomaso@gmail.com>
2021-12-27 14:27:54 -0300

    adding cast list

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -12,6 +12,8 @@ class MoviesProvider extends ChangeNotifier {
   List<Movie> nowPlayingMovies = [];
   List<Movie> popularMovies = [];
 
+  Map<int, List<Cast>> moviesCast = {};
+
   int _popularPage = 0;
 
   MoviesProvider() {
@@ -54,5 +56,15 @@ class MoviesProvider extends ChangeNotifier {
     popularMovies = [...popularMovies, ...popularResponse.results];
 
     notifyListeners();
+  }
+
+  Future<List<Cast>> getMovieCast(int movieId) async {
+    final jsonData = await _getJsonData('/3/movie/$movieId/credits');
+
+    final creditsResponse = CreditsResponse.fromJson(jsonData);
+
+    moviesCast[movieId] = creditsResponse.cast;
+
+    return creditsResponse.cast;
   }
 }
