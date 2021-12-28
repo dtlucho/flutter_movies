02d458e2f2a6f7ebf697560ea30fb8635ebad458
dtlucho <luciano.ditomaso@gmail.com>
2021-12-28 16:32:06 -0300

    using hero widget

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -83,14 +83,17 @@ class _PosterAndTitle extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 20.0),
       child: Row(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: FadeInImage(
-              placeholder: const AssetImage('assets/no-image.jpg'),
-              image: NetworkImage(movie.fullPosterImg),
-              height: 150,
-              width: 110,
-              fit: BoxFit.cover,
+          Hero(
+            tag: movie.heroId!,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: FadeInImage(
+                placeholder: const AssetImage('assets/no-image.jpg'),
+                image: NetworkImage(movie.fullPosterImg),
+                height: 150,
+                width: 110,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
           const SizedBox(width: 20.0),
